0df5ad8723e88e5191851f1b7a3f0bfe1c566cae
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2018-04-18 21:39:05 -0300

    Organização pro acompanhamento

--- js/global.js
+++ js/global.js
@@ -1,7 +1,19 @@
-
 jQuery(document).ready(function($) { 
+	//EFEITO DE SCROLL
 	$(".scroll").click(function(event){        
 		event.preventDefault();
 		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 600);
 	});
+
+	//MOSTRAR MENU MOBILE
+	$("#btn-menu").click(function(){
+			$("#menu").show();
+	});
+
+	//OCUTAR MENU MOBILE
+	$("#btn-close").click(function(){
+			$("#menu").hide();
+	});
 });
+
+		
