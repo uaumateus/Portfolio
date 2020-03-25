aef7c6ffed020941dc64f3c965d5fb4d609dab78
Mateus Santos <mateus.pereira99@hotmail.com>
2020-03-25 16:28:50 -0300

    Updates

---
+++ precache-manifest.91ef93d4d2d938f010160383dd08ded0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "540758872e280fc3683d83235ac61db2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7da1c40dfd03d5286f8a",
+    "url": "/Portfolio/static/css/main.7e883531.chunk.css"
+  },
+  {
+    "revision": "c57b16519f2845eda581",
+    "url": "/Portfolio/static/js/2.94700edf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Portfolio/static/js/2.94700edf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7da1c40dfd03d5286f8a",
+    "url": "/Portfolio/static/js/main.ff8659e7.chunk.js"
+  },
+  {
+    "revision": "6a58a16603ebe426610b",
+    "url": "/Portfolio/static/js/runtime-main.d3ebe2ed.js"
+  },
+  {
+    "revision": "efd28c594b278a4812c1a4516627295e",
+    "url": "/Portfolio/static/media/avatar.efd28c59.png"
+  },
+  {
+    "revision": "31a692424fe9242fc3e940b256d8a3aa",
+    "url": "/Portfolio/static/media/bubble.31a69242.svg"
+  },
+  {
+    "revision": "1c52d575f783fb01385a1fe83d992ed8",
+    "url": "/Portfolio/static/media/profile.1c52d575.png"
+  },
+  {
+    "revision": "345a957c3931698a19e9060bb45732e9",
+    "url": "/Portfolio/static/media/profile.345a957c.jpeg"
+  }
+]);
